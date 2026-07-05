95bb211badf27ebdf076d6dacfb5712d4824b958
Farhat Ijaz <farhatijazraza@gmail.com>
2026-07-05 11:06:49 +0500

    added gunicorn

--- main.py
+++ main.py
@@ -37,5 +37,8 @@ def dashboard():
     return render_template("dashboard.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
